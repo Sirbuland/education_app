8983c04bc33d62178e9856c2cf5fbb744bc69403
Sirbuland Atlas <sirbuland.atlas@gmail.com>
2018-08-22 13:57:41 +0500

    added redirection for home index according to user role

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
 
 	def index
-
+		if user_signed_in?
+			if current_user.role == 0
+				return redirect_to edit_student_path(id: current_user.id)
+			elsif current_user.role == 1
+				return redirect_to edit_tutor_path(id: current_user.id)
+			elsif current_user.super_admin
+				return redirect_to admin_index_path
+			end
+		end
 	end
+
 end

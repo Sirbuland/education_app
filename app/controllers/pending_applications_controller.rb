2db235830e6a4a8c397d9461aa0a8fe8baa320bf
Sirbuland Atlas <sirbuland.atlas@gmail.com>
2018-08-22 14:08:08 +0500

    added blank state for pending applications

--- app/controllers/pending_applications_controller.rb
+++ app/controllers/pending_applications_controller.rb
@@ -4,6 +4,7 @@ class PendingApplicationsController < ApplicationController
 	before_action :set_user, only: [:update]
 
   def index
+    @pending_applications_count = User.where(role: 1, activated: false).count
   	respond_to do |format|
 		  format.html
 		  format.json { render json: UserDatatable.new(view_context) }

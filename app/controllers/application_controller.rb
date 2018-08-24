d9ddffc7232e92db38c105cc6e390f0f4f74bbf8
Sirbuland Atlas <sirbuland.atlas@gmail.com>
2018-08-24 08:22:51 +0500

    fix student credits function in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_student_credits, if: [:is_student?]
+  before_action :set_student_credits, if: [:user_signed_in?, :is_student?]
 
   def set_student_credits
     @credits = current_user.packages.sum(:total_credits)
